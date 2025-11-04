d814c01688be26a8f21417cb3fae56c1e01451ad
makaraDevIt <makarasam68@gmail.com>
2025-11-04 14:25:10 +0700

    variable

--- Variable.java
+++ Variable.java
@@ -1,10 +1,24 @@
 public class Variable {
     public static void main(String[] args) {
-        int number =10;
-        float myFloat =5.5f;
-        char letter = 'a';
-        boolean myBool = false;
-        String text = "MK";
-        System.out.println(number + " " + myFloat + " " + letter + " " + myBool + " " + text);
+        // int number =10;
+        // float myFloat =5.5f;
+        // char letter = 'a';
+        // boolean myBool = false;
+        // String text = "MK";
+        // System.out.println(number + " " + myFloat + " " + letter + " " + myBool + " " + text);
+
+        // Displaying the variables
+        String  name = "makara";
+        System.out.println("Hello :"+ name);
+        // Print the value
+        int x =5;
+        int y=5;
+        System.out.println(x+y);
+        // mixing text and numbers
+       int a =1;
+       int b =2;
+       System.out.println("The sum is:"+a+b); // =12
+       System.out.println("The sum is:"+(a+b)); //=3
+
     }
 }
